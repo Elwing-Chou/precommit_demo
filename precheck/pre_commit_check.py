5a18c034d08bd101f994f994dec8488417a1f61e
Elwing-Chou <birdfan8814@gmail.com>
2025-09-10 01:31:03 +0800

    Update pre_commit_check.py

--- precheck/pre_commit_check.py
+++ precheck/pre_commit_check.py
@@ -1,35 +1,27 @@
 import os
-import glob
 import json
+import logging
+import precheck.constants as const
 import yaml
 import sys
 from cerberus import Validator
-import logging
 
-DEFAULT_CHECK_DIR = os.path.dirname(os.path.dirname(__file__)).replace("/.", "", 1)
-DEFAILT_SCHEMA = os.path.join(DEFAULT_CHECK_DIR, "conf_schema", "schema_fp.json")
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(levelname)s: %(message)s')
+# def get_all_yaml(root=YAML_DIR):
+#     path_join = os.path.join("**", "*.yaml")
+#     fp_list = glob.glob(path_join, recursive=True, root_dir=YAML_DIR)
+#     return fp_list
 
-
-def get_all_yaml(root=DEFAULT_CHECK_DIR):
-    path_join = os.path.join(root, "**", "*.yaml")
-    return glob.glob(path_join, recursive=True)
 
 if __name__ == "__main__":
-    logging.info("Custom hook started.")
-    logging.info(__file__)
-    logging.info(DEFAULT_CHECK_DIR)
-    logging.info(DEFAILT_SCHEMA)
-    yaml_list = get_all_yaml()
-
-    # with open(DEFAILT_SCHEMA, "r", encoding="utf-8") as f:
-    #     ref_list = json.load(f)
-
+    logging.info("YAML check started.")
+    logging.info(f"args: {sys.argv}")
+    yaml_list = sys.argv[1:]
 
     for yaml_fp in yaml_list:
-        logging.info(yaml_fp)
-        ref_fp = yaml_fp.replace("network_configs", "conf_schema/schema/network_configs")
+        logging.info(f"Check {yaml_fp}")
+        ref_fp = os.path.join(const.YAML_SCHEMA_SAVE_DIR, yaml_fp)
         ref_fp = ref_fp.replace(".yaml", ".json")
         with open(ref_fp, "r", encoding="utf-8") as f:
             validate_schema = json.load(f)
@@ -38,7 +30,6 @@ if __name__ == "__main__":
         with open(yaml_fp, "r", encoding="utf-8") as f:
             data = yaml.safe_load(f)
         result = v.validate(data)
-        #logging.info(result)
         if result is False:
             logging.info(v.errors)
             raise SystemExit(1)
