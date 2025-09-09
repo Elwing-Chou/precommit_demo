ee803abd35e55ba2975889765a6fd8e466cae913
Elwing-Chou <birdfan8814@gmail.com>
2025-09-09 16:55:07 +0800

    Update check_test.py

--- check_test.py
+++ check_test.py
@@ -1,7 +1,9 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    stream=sys.stderr,
+                    format='%(levelname)s: %(message)s')
 
 
 if __name__ == "__main__":
