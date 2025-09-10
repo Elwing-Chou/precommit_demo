0c37ed2c9b24663109992090000d4162af4649eb
Elwing-Chou <birdfan8814@gmail.com>
2025-09-10 15:29:23 +0800

    Update pattern_func.py

--- precheck/pattern_func.py
+++ precheck/pattern_func.py
@@ -1,6 +1,9 @@
 # define func for cerberus check
+import logging
+
 
 def check_freeform_exists(field, value, error):
     # need revise
+    logging.info(f"check freeform {field} {value} {error}")
     if "Freeform Config" not in value:
         error(field, "Path Invalid")
