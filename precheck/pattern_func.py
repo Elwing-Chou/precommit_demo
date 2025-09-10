b20f82d8546fe258b01c01bfe6b3cb6a08dc7cec
Elwing-Chou <birdfan8814@gmail.com>
2025-09-10 15:30:53 +0800

    Update pattern_func.py

--- precheck/pattern_func.py
+++ precheck/pattern_func.py
@@ -3,7 +3,7 @@ import logging
 
 
 def check_freeform_exists(field, value, error):
-    # need revise
+    # need revise: check path
     logging.info(f"check freeform {field} {value} {error}")
     if "Freeform Config" not in value:
         error(field, "Path Invalid")
